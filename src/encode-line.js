83dbbba20ce698ae20ba882bf31b38003322cf7d
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-10-24 16:38:07 +0200

    feat: resolve incode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if (typeof str !== 'string') return false;
+  result = [];
+  str.split('').reduce((accum, char, i) => {
+    if (accum === null) {
+      result.push(1);
+      return char;
+    }
+    if (char === accum) {
+      result[result.length - 1]++;
+    } else {
+      result.push(accum);
+      result.push(1);
+    }
+    if (i === str.length - 1) result.push(char)
+    return char;
+  }, null);
+  return result.filter(el => el !== 1).join('');
 }
 
 module.exports = {
