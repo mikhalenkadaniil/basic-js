c3db36019537c8f752e5b3d2e2b79d9b71f8810a
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-10-25 19:57:30 +0200

    feat: resolve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,31 @@ const { NotImplementedError } = require('../lib');
  *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    this.chain.push(`${value}`);
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (typeof position !== 'number' || position < 0 || position > this.chain.length) {
+      this.chain = [];
+      return "You can\'t remove incorrect link!";
+    }
+    this.chain.splice(position - 1, 1);
+    return chainMaker;
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return chainMaker;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const bufferChain = [...this.chain];
+    this.chain = [];
+    return `( ${bufferChain.join(' )~~( ')} )`;
   },
 };
 
